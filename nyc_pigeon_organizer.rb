f22d0a18e3b628d080041a482cb05e9130709746
Shelley Brook <sbrook13@gmail.com>
2020-07-19 19:22:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key,value), final_array|
+  final_results = data.each_with_object({}) do |(key,value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
@@ -12,10 +12,8 @@ def nyc_pigeon_organizer(data)
         end
         final_array[name][key].push(inner_key.to_s)
       end
-    final_array[k] = v
+    end 
   end
-  
-binding.pry
 end
 
 

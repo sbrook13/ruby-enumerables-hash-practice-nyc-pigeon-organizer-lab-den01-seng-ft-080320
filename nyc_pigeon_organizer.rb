a8d67c3b066083e7355ab82c9fc68eb1fd8b45c1
Shelley Brook <sbrook13@gmail.com>
2020-07-19 19:20:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,39 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.invert
-  data.find_all {|k,v| 
-pigeon_list[a]=b
-pigeon_list
+  data.each_with_object({}) do |(key,value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key.to_s)
+      end
+    final_array[k] = v
+  end
+  
 binding.pry
 end
+
+
+# pigeon_data = {
+#  :color => {
+#    :purple => ["Theo", "Peter Jr.", "Lucky"],
+#    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#    :brown => ["Queenie", "Alex"]
+#  },
+#  :gender => {
+#    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#    :female => ["Queenie", "Ms. K"]
+#  },
+#  :lives => {
+#   "Subway" => ["Theo", "Queenie"],
+#    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#    "Library" => ["Peter Jr."],
+#    "City Hall" => ["Andrew"]
+#  }
+#}
